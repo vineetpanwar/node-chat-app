f694978d5cc0dbc17a0302b47bac86806cb3e64d
Vineet panwar <vineetpanwar027@gmail.com>
2018-09-17 22:12:23 +0530

    'addedtimeformating'

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,19 @@
+function scrollToBottom(){
+//selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+//heights
+var clientHeight = messages.prop('clientHeight');
+var scrollTop = messages.prop('scrollTop');
+var scrollHeight = messages.prop('scrollHeight');
+var newMessageHeight = newMessage.innerHeight();
+var lastMessageHeight = newMessage.prev().innerHeight();
+
+if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=scrollHeight){
+  messages.scrollTop(scrollHeight);
+}
+}
+
 var socket = io();
 socket.on('connect',function() {
   console.log('Connected to server');
@@ -13,6 +29,7 @@ socket.on('newMessage',function(msg){
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
   /*
   var li=jQuery('<li></li>');
   li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
@@ -30,6 +47,7 @@ socket.on('newLocationMessage',function(msg){
   })
 
   jQuery('#messages').append(html);
+  scrollToBottom();
   /*
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My Current Location</a>');
