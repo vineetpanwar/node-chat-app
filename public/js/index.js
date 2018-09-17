4b211831f7dc6028dcbc6a6fbd1d3847b803d7a4
Vineet panwar <vineetpanwar027@gmail.com>
2018-09-17 20:44:03 +0530

    'addedmustachejs'

--- public/js/index.js
+++ public/js/index.js
@@ -5,19 +5,39 @@ socket.on('connect',function() {
 
 socket.on('newMessage',function(msg){
   var formattedTime = moment(msg.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template,{
+    text:msg.text,
+    from:msg.from,
+    createdAt:formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  /*
   var li=jQuery('<li></li>');
   li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
   jQuery('#messages').append(li);
+  */
 });
 
 socket.on('newLocationMessage',function(msg){
   var formattedTime = moment(msg.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template,{
+    url:msg.url,
+    from:msg.from,
+    createdAt:formattedTime
+  })
+
+  jQuery('#messages').append(html);
+  /*
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My Current Location</a>');
   li.text(`${msg.from} ${formattedTime}: `);
   a.attr('href',msg.url);
   li.append(a);
   jQuery('#messages').append(li);
+  */
 });
 
 socket.on('disconnect',function() {
